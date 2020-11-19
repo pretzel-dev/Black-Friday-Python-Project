1c0fcecc779ac78ed7354f0fa98cd2216565c4f8
Pretzel <precelek.precelkowski@gmail.com>
2020-11-19 13:00:56 +0100

    Update of amazon-tracker
    - Fixed prices
    - Printing all prices of first site

--- Lowest-Product-Price/Source/amazon-tracker.py
+++ Lowest-Product-Price/Source/amazon-tracker.py
@@ -1,20 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
 def track(productName):
+
+    get_price = False
+
     headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0'}
     URL = 'https://www.amazon.com/s?k=' + productName
-    page = requests.get(URL, headers=headers)
-    beautiful_soup = BeautifulSoup(page.content, 'html.parser')
-    prices = []
 
-    spans = beautiful_soup.find_all('span', {'class': 'a-price-whole'})
+    while get_price == False:
+        page = requests.get(URL, headers=headers)
+        beautiful_soup = BeautifulSoup(page.content, 'html.parser')
+        prices = []
 
-    for span in spans:
-        prices.append(span.get_text())
+        spans = beautiful_soup.find_all(class_ = 'a-price-whole')
+
+
+
+        for span in spans:
+            prices.append(float(span.get_text().replace('.', '').replace(',','')))
+            get_price = True
+
+        if(get_price == True):
+            print(prices)
+        else:
+            print("Please wait, this can take a moment...")
+        
+        time.sleep(1)
     
-    print(prices)
-
 
 
 track('Macbook')
